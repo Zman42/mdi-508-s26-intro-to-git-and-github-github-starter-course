797cefb6e109623f12b68116acc2e3fb14e0ed0c
Zman42 <zach.reifsteck@gmail.com>
2026-02-08 02:03:26 +0000

    Task4-Attempt2

--- sum_csv.py
+++ sum_csv.py
@@ -1,9 +1,9 @@
 import sys
 import csv
 
-number_sum.csv = sys.argv[1]
+filename = sys.argv[1]
 
-with open(number_sum.csv, "r") as f:
+with open(filename, "r") as f:
     reader = csv.reader(f)
     row = next(reader)
     numbers = [int(x) for x in row]
